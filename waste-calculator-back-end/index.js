660d03ffdd9c9fd352b368f878a3486f600a9e27
Milen Gardev <milenford@gmail.com>
2020-03-02 00:33:56 +0100

    Update index.js

--- waste-calculator-back-end/index.js
+++ waste-calculator-back-end/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('It works'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/', (req, res) => res.send('It works'));
+
+
+app.get('/result', function (req, res) {
+    const result = {
+        data : {
+            id: '1231'
+        }
+    };
+
+    res.send(result);
+});
